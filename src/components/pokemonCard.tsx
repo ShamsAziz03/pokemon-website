1c1827fa0683dd1c89dd2674ff2210f334f557eb
ShamsAziz03 <shamsimad550@gmail.com>
2026-03-24 09:06:19 +0200

    feat: add the basic structure for pokemon website, setup tailwind

---
+++ src/components/pokemonCard.tsx
@@ -0,0 +1,12 @@
+function PokemonCard() {
+  return (
+    <div className="flex flex-col gap-8">
+      <h2 className="text-xl font-bold bg-red-600">Pikachu</h2>
+      <h2 className="text-xl font-bold bg-red-600">Pikachu</h2>
+      <h2 className="text-xl font-bold bg-red-600">Pikachu</h2>
+      <h2 className="text-xl font-bold bg-red-600">Pikachu</h2>
+    </div>
+  );
+}
+
+export default PokemonCard;
