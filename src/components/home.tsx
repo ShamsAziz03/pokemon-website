96d10c5e7c991fcad0d81a54fd9187d5e28f25a1
ShamsAziz03 <shamsimad550@gmail.com>
2026-04-06 21:52:47 +0300

    refactor: implement offset pagination for pokemon list instead of loop

--- src/components/home.tsx
+++ src/components/home.tsx
@@ -7,95 +7,110 @@ import { FaHeart, FaSearch } from "react-icons/fa";
 import { useNavigate } from "react-router-dom";
 import PokemonCard from "../components/pokemonCard";
 
-type Pokemon = {
-	img: string;
+// type Pokemon = {
+// 	img: string;
+// 	name: string;
+// 	id: string;
+// 	types: string[];
+// 	height: string;
+// 	weight: string;
+// 	abilities: string;
+// };
+
+export type Pokemon = {
 	name: string;
-	id: string;
-	types: string[];
-	height: string;
-	weight: string;
-	abilities: string;
+	url: string;
 };
+
+export function search(data: string, pokemonList: Pokemon[]) {
+	const names = pokemonList?.map((pokemon) => pokemon.name) || [];
+	const fuse = new Fuse(names);
+	let filtered = [...(pokemonList || [])];
+	if (!data.trim()) {
+		filtered = [...(pokemonList || [])];
+	} else {
+		const findPokemons = fuse.search(data).map((result) => result.item);
+		filtered = (pokemonList || []).filter((pokemon) =>
+			findPokemons.includes(pokemon.name),
+		);
+	}
+	return filtered;
+}
+
+// export async function getPokemon() {
+// 	const randomId = Math.floor(Math.random() * 1025) + 1;
+// 	const details = await Axios.get(
+// 		`https://pokeapi.co/api/v2/pokemon/${randomId}`,
+// 	);
+
+// 	const types: string[] = details.data.types.map(
+// 		(type: { type: { name: string } }) => type.type.name.toUpperCase(),
+// 	);
+
+// 	const pokemonDetails = {
+// 		img: details.data.sprites.front_default,
+// 		name: details.data.name.toUpperCase(),
+// 		id: `# ${details.data.id}`,
+// 		types: types,
+// 		height: `${details.data.height} dm`,
+// 		weight: `${details.data.weight} lbs`,
+// 		abilities: details.data.abilities
+// 			.map((a: { ability: { name: string } }) => a.ability.name)
+// 			.join(", "),
+// 	};
+// 	return pokemonDetails;
+// }
+
+export async function getPokemon() {
+	const randomId = Math.floor(Math.random() * 1025) + 1;
+	const details = await Axios.get(
+		`https://pokeapi.co/api/v2/pokemon/?limit=1&offset=${randomId}`,
+	);
+	return details.data.results[0];
+}
+
+export async function getPokemonsList() {
+	const randomId = Math.floor(Math.random() * 1025) + 1;
+	const details = await Axios.get(
+		`https://pokeapi.co/api/v2/pokemon/?limit=6&offset=${randomId}`,
+	);
+	console.log(JSON.stringify(details.data.results, null, 2));
+	return details.data.results;
+}
+
+export const pokemons = (searchText: string, pokemonList: Pokemon[]) => {
+	if (!searchText) return pokemonList || [];
+	return search(searchText, pokemonList);
+};
+
+export async function storePokemonOfDay() {
+	const result = await getPokemon();
+	const d = new Date();
+	let day = d.getDay();
+	const pokemonObj = { day: day, pokemonDetails: { ...result } };
+	localStorage.setItem("currentPokemon", JSON.stringify(pokemonObj));
+	return pokemonObj.pokemonDetails;
+}
+
+export function getPokemonOfDay() {
+	const currentPokemon = localStorage.getItem("currentPokemon");
+	if (!currentPokemon) {
+		return storePokemonOfDay();
+	} else {
+		const currentPokemonDetails = JSON.parse(currentPokemon);
+		const day = new Date().getDay();
+		if (day === currentPokemonDetails.day) {
+			return currentPokemonDetails.pokemonDetails;
+		} else {
+			//means the stored one is from diff day
+			return storePokemonOfDay();
+		}
+	}
+}
 
 function Home() {
 	const [searchText, setSearchText] = useState("");
 	const navigate = useNavigate();
-
-	function search(data: string) {
-		const names = pokemonList?.map((pokemon) => pokemon.name) || [];
-		const fuse = new Fuse(names);
-		let filtered = [...(pokemonList || [])];
-		if (!data.trim()) {
-			filtered = [...(pokemonList || [])];
-		} else {
-			const findPokemons = fuse.search(data).map((result) => result.item);
-			filtered = (pokemonList || []).filter((pokemon) =>
-				findPokemons.includes(pokemon.name),
-			);
-		}
-		return filtered;
-	}
-
-	async function getPokemon() {
-		const randomId = Math.floor(Math.random() * 1025) + 1;
-		const details = await Axios.get(
-			`https://pokeapi.co/api/v2/pokemon/${randomId}`,
-		);
-
-		const types: string[] = details.data.types.map(
-			(type: { type: { name: string } }) => type.type.name.toUpperCase(),
-		);
-
-		const pokemonDetails = {
-			img: details.data.sprites.front_default,
-			name: details.data.name.toUpperCase(),
-			id: `# ${details.data.id}`,
-			types: types,
-			height: `${details.data.height} dm`,
-			weight: `${details.data.weight} lbs`,
-			abilities: details.data.abilities
-				.map((a: { ability: { name: string } }) => a.ability.name)
-				.join(", "),
-		};
-		return pokemonDetails;
-	}
-
-	async function getPokemonsList() {
-		const list = ["", "", "", "", "", ""];
-		const promises = list.map(getPokemon);
-		const result = await Promise.all(promises);
-		return result.flat();
-	}
-
-	const pokemons = () => {
-		if (!searchText) return pokemonList || [];
-		return search(searchText);
-	};
-
-	async function storePokemonOfDay() {
-		const result = await getPokemon();
-		const d = new Date();
-		let day = d.getDay();
-		const pokemonObj = { day: day, pokemonDetails: { ...result } };
-		localStorage.setItem("currentPokemon", JSON.stringify(pokemonObj));
-		return pokemonObj.pokemonDetails;
-	}
-
-	function getPokemonOfDay() {
-		const currentPokemon = localStorage.getItem("currentPokemon");
-		if (!currentPokemon) {
-			return storePokemonOfDay();
-		} else {
-			const currentPokemonDetails = JSON.parse(currentPokemon);
-			const day = new Date().getDay();
-			if (day === currentPokemonDetails.day) {
-				return currentPokemonDetails.pokemonDetails;
-			} else {
-				//means the stored one is from diff day
-				return storePokemonOfDay();
-			}
-		}
-	}
 
 	const {
 		isPending: pokemonOfDayPending,
@@ -187,9 +202,9 @@ function Home() {
 
 			<section className="grid grid-cols-[repeat(auto-fit,minmax(300px,1fr))] gap-20 p-10">
 				{pokemonOfDay && <PokemonCard pokemonDetails={pokemonOfDay} />}
-				{pokemons()?.map((pokemon: Pokemon, index) => {
+				{pokemons(searchText, pokemonList)?.map((pokemon: Pokemon) => {
 					return (
-						<div key={index}>
+						<div key={pokemon.name}>
 							<PokemonCard pokemonDetails={pokemon} />
 						</div>
 					);
--- src/components/pokemonCard.tsx
+++ src/components/pokemonCard.tsx
@@ -1,6 +1,9 @@
+import { useQuery } from "@tanstack/react-query";
+import Axios from "axios";
 import { useNavigate } from "react-router-dom";
+import type { Pokemon } from "./home";
 
-type Pokemon = {
+type PokemonData = {
 	img: string;
 	name: string;
 	id: string;
@@ -10,21 +13,72 @@ type Pokemon = {
 	abilities: string;
 };
 
+export async function getPokemonInfo(url: string): Promise<PokemonData> {
+	const details = await Axios.get(url);
+
+	const types: string[] = details.data.types.map(
+		(type: { type: { name: string } }) => type.type.name.toUpperCase(),
+	);
+
+	const pokemonDetails = {
+		img: details.data.sprites.front_default,
+		name: details.data.name.toUpperCase(),
+		id: `# ${details.data.id}`,
+		types: types,
+		height: `${details.data.height} dm`,
+		weight: `${details.data.weight} lbs`,
+		abilities: details.data.abilities
+			.map((a: { ability: { name: string } }) => a.ability.name)
+			.join(", "),
+	};
+	return pokemonDetails;
+}
+
 function PokemonCard({ pokemonDetails }: { pokemonDetails: Pokemon }) {
 	const navigate = useNavigate();
 
+	const {
+		isPending,
+		error,
+		data: pokemonInfo,
+	} = useQuery({
+		queryKey: ["pokemonInfo", pokemonDetails.url],
+		queryFn: () => getPokemonInfo(pokemonDetails.url),
+		refetchOnWindowFocus: false,
+	});
+
+	if (isPending)
+		return (
+			<h1
+				className="row-start-1 row-end-2 col-start-1 col-end-3 font-bold text-gray-800 text-2xl"
+				style={{ textShadow: "2px 2px 4px rgba(0,0,0,0.3)" }}
+			>
+				Loading...
+			</h1>
+		);
+
+	if (error)
+		return (
+			<h1
+				className="row-start-1 row-end-2 col-start-1 col-end-3 font-bold text-gray-800 text-2xl"
+				style={{ textShadow: "2px 2px 4px rgba(0,0,0,0.3)" }}
+			>
+				An error has occurred: {error?.message}
+			</h1>
+		);
+
 	return (
-		<div className="flex flex-col rounded-[20px] shadow-xl border-2 border-black justify-center items-center hover:[transform:translateY(-10px)]">
+		<div className="flex flex-col rounded-[20px] shadow-xl border-2 border-black justify-center items-center relative top-0 transition-[top] duration-[350ms] ease-in-out hover:-top-[10px]">
 			<img
 				alt="how the pokemon looks like"
 				className="w-[50%] h-[200px] border-b-2"
-				src={pokemonDetails.img}
+				src={pokemonInfo.img}
 			/>
 			<section className="flex flex-col gap-2 justify-start items-start pl-8 pr-8 pb-3 w-[100%]">
-				<p className="text-md font-bold text-gray-500">{pokemonDetails.id}</p>
-				<p className="text-xl font-bold">{pokemonDetails.name}</p>
+				<p className="text-md font-bold text-gray-500">{pokemonInfo.id}</p>
+				<p className="text-xl font-bold">{pokemonInfo.name}</p>
 				<div className="flex flex-wrap gap-4">
-					{pokemonDetails.types.map((type) => {
+					{pokemonInfo.types.map((type) => {
 						return (
 							<p
 								className="text-xs font-semibold text-black bg-gray-300 rounded-[10px] shadow-xl p-1 pl-4 pr-4"
@@ -38,7 +92,7 @@ function PokemonCard({ pokemonDetails }: { pokemonDetails: Pokemon }) {
 				<button
 					className="text-md font-semibold text-white bg-gray-800 rounded-[10px] shadow-xl w-[100%] p-1 mt-5 hover:bg-gray-400 border-2 hover:shadow-2xl hover:text-black"
 					onClick={() => {
-						navigate("/aboutPokemon", { state: { ...pokemonDetails } });
+						navigate("/aboutPokemon", { state: { ...pokemonInfo } });
 					}}
 					type="button"
 				>
